aaa001fb78e30a5f17fd4ab3aa42d8a4b2ec159e
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2023-05-26 16:39:04 +0700

    mencari node dari depan dengan if kondisi

--- double_lingked_list/double_lingked_list.cpp
+++ double_lingked_list/double_lingked_list.cpp
@@ -17,7 +17,7 @@ public:
 
 	void addNode();
 	bool search(int rolNo, Node** previous, Node** current);
-	bool deNode(int rollNo);
+	bool delNode(int rollNo);
 	bool listEmpty();
 	void traverse();
 	void revtraverse();
@@ -36,5 +36,19 @@ void DoubleLingkedList::addNode() {
 	Node* newNode = new Node(); //step 1
 	newNode->noMhs = nim; //step2
 	newNode->name = nm; //step 2
+
+	/*insert a node ind the begining of a doubly - lingked list*/
+	if (START == NULL || nim <= START->noMhs) { //check if data null
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nduplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START; //Step 3
+		if (START != NULL)
+			START->prev = newNode;	//step 4
+		newNode->prev = NULL;	//
+		START = newNode;	//step 6
+		return;
+	}
 
 	
